eea99b7360bf1cd9b04cbd1e3bbfc7a72c0c71e8
Pascal Müller <privat@pascal-mueller.ch>
2018-04-19 22:12:57 +0200

    ConnectActivity: Implemented Searching for Frackstock with High RSSI value, ready for connecting.

--- AndroidApp/Frackstock/app/src/main/java/ch/pascal_mueller/frackstock/ConnectActivity.java
+++ AndroidApp/Frackstock/app/src/main/java/ch/pascal_mueller/frackstock/ConnectActivity.java
@@ -1,13 +1,152 @@
 package ch.pascal_mueller.frackstock;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.TimerTask;
 
 public class ConnectActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 2;
+
+    private UartService uartService = null;
+    private BluetoothAdapter bluetoothAdapter = null;
+    BluetoothLeScanner bluetoothLeScanner = null;
+    private TextView textView_Info = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(this, "Bluetooth is not available!", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        textView_Info = (TextView) findViewById(R.id.textView1);
+
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This app needs location access");
+            builder.setMessage("Please grant location access so this app can use Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                public void onDismiss(DialogInterface dialog) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                }
+            });
+            builder.show();
+        }
+        else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            }
+            else {
+                new CountDownTimer(500, 1) {
+                    public void onTick(long l) {
+                    }
+
+                    public void onFinish() {
+                        startConnecting();
+                    }
+                }.start();
+            }
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_COARSE_LOCATION:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (!bluetoothAdapter.isEnabled()) {
+                        Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+                    }
+                    else
+                    {
+                        startConnecting();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(this, "BluetoothLE is not available!", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case REQUEST_ENABLE_BT:
+                if(resultCode == RESULT_OK)
+                    startConnecting();
+                else
+                    finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void startConnecting()
+    {
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        if(bluetoothLeScanner != null)
+        {
+            textView_Info.setText("Searching for Frackstock with high RSSI.\nHold your Smartphone next to your Stick...");
+            bluetoothLeScanner.startScan(scanCallback);
+        }
+        else
+        {
+            Toast.makeText(this, "BluetoothLE is not available!", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            if(result.getRssi() > -45)
+            {
+                if(result.getDevice().getName().compareTo("Frackstock") == 0) {
+                    String message = "Found " + result.getDevice().getAddress()+ ", connecting...";
+                    textView_Info.setText(message);
+                    bluetoothLeScanner.stopScan(scanCallback);
+
+                }
+            }
+            super.onScanResult(callbackType, result);
+        }
+    };
 }
