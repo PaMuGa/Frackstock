55a6fa31918cc41827a425cabc8b8fbb35e021f5
Pascal Müller <privat@pascal-mueller.ch>
2018-04-21 21:58:02 +0200

    ConnectActivity: Auto connecting implemented. Button for communication testing inserted.

--- AndroidApp/Frackstock/app/src/main/java/ch/pascal_mueller/frackstock/ConnectActivity.java
+++ AndroidApp/Frackstock/app/src/main/java/ch/pascal_mueller/frackstock/ConnectActivity.java
@@ -6,34 +6,52 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.CountDownTimer;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.TimerTask;
 
 public class ConnectActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 2;
 
-    private UartService uartService = null;
-    private BluetoothAdapter bluetoothAdapter = null;
-    BluetoothLeScanner bluetoothLeScanner = null;
     private TextView textView_Info = null;
+    private Button button_test = null;
+
+    private BluetoothAdapter bluetoothAdapter = null;
+    private BluetoothLeScanner bluetoothLeScanner = null;
+    private BluetoothDevice bluetoothDevice = null;
+    private UartService uartService = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connect);
+
+        setLayoutConnect();
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        service_init();
 
         if(bluetoothAdapter == null)
         {
@@ -42,7 +60,7 @@ public class ConnectActivity extends AppCompatActivity {
             return;
         }
 
-        textView_Info = (TextView) findViewById(R.id.textView1);
+
 
         if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             final AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -72,6 +90,35 @@ public class ConnectActivity extends AppCompatActivity {
                 }.start();
             }
         }
+    }
+
+    private void setLayoutConnect()
+    {
+        setContentView(R.layout.activity_connect);
+        textView_Info = (TextView) findViewById(R.id.textView1);
+
+        button_test = null;
+    }
+
+    private void setLayoutControl()
+    {
+        setContentView(R.layout.activity_control);
+        textView_Info = null;
+
+        button_test = (Button) findViewById(R.id.button_test);
+        button_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    byte[] value = "123".getBytes("UTF-8");
+                    uartService.writeRXCharacteristic(value);
+                } catch (UnsupportedEncodingException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -144,9 +191,150 @@ public class ConnectActivity extends AppCompatActivity {
                     textView_Info.setText(message);
                     bluetoothLeScanner.stopScan(scanCallback);
 
+                    bluetoothDevice = result.getDevice();
+                    uartService.connect(bluetoothDevice.getAddress());
                 }
             }
             super.onScanResult(callbackType, result);
+        }
+    };
+
+    private final BroadcastReceiver UARTStatusChangeReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            final Intent mIntent = intent;
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_CONNECTED)) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        String message = "Connected to Frackstock (" + bluetoothDevice.getAddress() + ")";
+                        textView_Info.setText(message);
+
+                        new CountDownTimer(500, 1) {
+                            public void onTick(long l) {
+                            }
+
+                            public void onFinish() {
+                                setLayoutControl();
+
+
+                                //setContentView(R.layout.activity_control);
+
+
+                            }
+                        }.start();
+
+                        //String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                        //Log.d(TAG, "UART_CONNECT_MSG");
+                        //btnConnectDisconnect.setText("Disconnect");
+                        //edtMessage.setEnabled(true);
+                        //btnSend.setEnabled(true);
+                        //((TextView) findViewById(R.id.deviceName)).setText(mDevice.getName()+ " - ready");
+                        //listAdapter.add("["+currentDateTimeString+"] Connected to: "+ mDevice.getName());
+                        //messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);
+                        //mState = UART_PROFILE_CONNECTED;
+                    }
+                });
+            }
+
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_DISCONNECTED)) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        //String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                        /*Log.d(TAG, "UART_DISCONNECT_MSG");
+                        btnConnectDisconnect.setText("Connect");
+                        edtMessage.setEnabled(false);
+                        btnSend.setEnabled(false);
+                        ((TextView) findViewById(R.id.deviceName)).setText("Not Connected");
+                        listAdapter.add("["+currentDateTimeString+"] Disconnected to: "+ mDevice.getName());
+                        mState = UART_PROFILE_DISCONNECTED;*/
+                        uartService.close();
+                        setLayoutConnect();
+                        textView_Info.setText("Disconnected.");
+
+                        new CountDownTimer(500, 1) {
+                            public void onTick(long l) {
+                            }
+
+                            public void onFinish() {
+                                startConnecting();
+                            }
+                        }.start();
+                        //setUiState();
+
+                    }
+                });
+            }
+
+
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_SERVICES_DISCOVERED)) {
+                uartService.enableTXNotification();
+            }
+            //*********************//
+            if (action.equals(UartService.ACTION_DATA_AVAILABLE)) {
+
+                final byte[] txValue = intent.getByteArrayExtra(UartService.EXTRA_DATA);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        try {
+                            String text = new String(txValue, "UTF-8");
+                            //String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+                            /*listAdapter.add("["+currentDateTimeString+"] RX: "+text);
+                            messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);*/
+
+                        } catch (Exception e) {
+                            //Log.e(TAG, e.toString());
+                        }
+                    }
+                });
+            }
+            //*********************//
+            if (action.equals(UartService.DEVICE_DOES_NOT_SUPPORT_UART)){
+                //showMessage("Device doesn't support UART. Disconnecting");
+                uartService.disconnect();
+                startConnecting();
+            }
+
+
+        }
+    };
+
+    private void service_init() {
+        Intent bindIntent = new Intent(this, UartService.class);
+        bindService(bindIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(UARTStatusChangeReceiver, makeGattUpdateIntentFilter());
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(UartService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(UartService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(UartService.DEVICE_DOES_NOT_SUPPORT_UART);
+        return intentFilter;
+    }
+
+    //UART service connected/disconnected
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+            uartService = ((UartService.LocalBinder) rawBinder).getService();
+            //Log.d(TAG, "onServiceConnected mService= " + mService);
+            if (!uartService.initialize()) {
+                //Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+
+        }
+
+        public void onServiceDisconnected(ComponentName classname) {
+            ////     mService.disconnect(mDevice);
+            uartService = null;
         }
     };
 }
